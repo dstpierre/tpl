eaf437ff347a03ed236359763c25acf4dca7961b
Dominic St-Pierre <dominicstpierre@gmail.com>
2024-07-11 11:24:58 -0400

    added a map function to pass dynamic params to a sub template

--- funcmap.go
+++ funcmap.go
@@ -1,8 +1,11 @@
 package tpl
 
+import "fmt"
+
 func enhanceFuncMap(fmap map[string]any) {
 	addTranslationFunctions(fmap)
 	addInternationalizationFunctions(fmap)
+	addHelperFunctions(fmap)
 }
 
 func addTranslationFunctions(fmap map[string]any) {
@@ -15,4 +18,24 @@ func addTranslationFunctions(fmap map[string]any) {
 func addInternationalizationFunctions(fmap map[string]any) {
 	fmap["shortdate"] = ToDate
 	fmap["currency"] = ToCurrency
+}
+
+func addHelperFunctions(fmap map[string]any) {
+	fmap["map"] = func(v ...any) map[string]any {
+		if len(v)%2 != 0 {
+			panic("call to map should have a key and value of even pairs")
+		}
+
+		m := make(map[string]any)
+		for i := 0; i < len(v); i += 2 {
+			key, ok := v[i].(string)
+			if !ok {
+				panic(fmt.Sprintf("key for the map function should be string: %v", v[i]))
+			}
+
+			m[key] = v[i+1]
+		}
+
+		return m
+	}
 }
